5ddebb43d191a9c0ef9d0de61f6135186c0ce763
Mounir Jurde <mounir_jurde95@hotmail.com>
2022-01-19 20:45:40 +0200

    deleted typescript files from react branch

--- TypeScript Files/src/checkoutorder.ts
+++
@@ -1,9 +0,0 @@
-import { TObjectArray } from './enum.d';
-import displayfun from './displayfunc';
-
-/////////// CHECKOUT ORDER //////////////////////
-let checkoutorder =(finalArrayObject: Required<TObjectArray[]>) => {
-    console.log(displayfun(finalArrayObject,'$'));
-};
-
-export default checkoutorder;
